32653f3e762aaf49a5ec2ee3a3000765b41403ad
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2018-12-18 12:19:33 -0600

    added php

---
+++ form-to-email.php
@@ -0,0 +1,5 @@
+<?php
+if($_POST["Email"]) {
+    mail("normedorconsulting@gmail.com", "Form to email message", $_POST["Name" and "Email" and "Subject" and "Comment"], "From: nickgonzales76017@gmail.com");
+}
+?>
