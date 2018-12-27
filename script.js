8fa8b8ec2e1be8693171a04da096df6f58b9405b
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2018-12-27 16:12:15 -0600

    Snack bar attempt 1 (js)

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName) {
   var i, x;
